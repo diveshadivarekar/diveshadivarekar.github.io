dc8a966ba826025aa59322383d5ce26af9c4b524
Divesh Adivarekar <diveshadivarekar09@gmail.com>
2024-11-08 21:29:25 +0530

    Revert "created and added header component"
    This reverts commit 4e050619a84b9562c86b4e3bd82cd96755a6bd7e.

--- js/component.js
+++ js/component.js
@@ -1,42 +1,8 @@
 // for dynamically adding the components to the page 
 
-//header component
-// Function to load the header HTML and insert it into the page
-function loadHeader() {
-    setTimeout(loadHeader, 100);
-    const timestamp = new Date().getTime(); // Get the current timestamp
-    const headerUrl = `./component/header.html?v=${Date.now()}`; // Append timestamp to URL
-    fetch(headerUrl)
-        .then(response => response.text())
-        .then(data => {
-            // Insert the fetched HTML into the beginning of the body or another target element
-            document.getElementById("header").innerHTML = data;
-
-            // After inserting the HTML, set the active class
-            setActiveNavLink();
-        })
-        .catch(error => console.error('Error loading header:', error));
-}
-
-// Function to set the active class on the current page link
-function setActiveNavLink() {
-    const currentPath = window.location.pathname;
-    const navLinks = document.querySelectorAll('.fh5co-nav ul li a');
-
-    navLinks.forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
-            link.parentElement.classList.add('active');
-        } else {
-            link.parentElement.classList.remove('active');
-        }
-    });
-}
-
-document.addEventListener('DOMContentLoaded',loadHeader());
-
 
 // footer component 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     // Fetch and load the footer HTML
     fetch('./component/footer.html')
         .then(response => response.text())
